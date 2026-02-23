4dfbc8ce016a608b4c3b2b41ddb0e0516325d7d2
nancykhalid88 <42310142.Nancy@acu.edu.eg>
2026-02-23 22:52:29 +0200

    Q1

--- Assignment03/Program.cs
+++ Assignment03/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //A- 1-attribute should be private    2-No validation in the method
+            //B- make the attributes private and add validation to the method
+            //C-Anyone can access the data and change it
+
+            #endregion
         }
     }
 }
