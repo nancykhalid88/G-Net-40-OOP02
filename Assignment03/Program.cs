3ed14b4b8efe12a8568a83d53fd98deb194e226c
nancykhalid88 <42310142.Nancy@acu.edu.eg>
2026-02-23 23:02:19 +0200

    Q2

--- Assignment03/Program.cs
+++ Assignment03/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Assignment03
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment03
 {
     internal class Program
     {
@@ -10,6 +13,25 @@
             //C-Anyone can access the data and change it
 
             #endregion
-        }
+            #region Q2
+            //What is the difference between a field and a property in C#? Can a property contain logic? 
+            //Give an example of a read-only property that returns a calculated value.
+
+            //A feild is a variable that can be assigned a value, it doesnt have validation or constrints on it
+            //A property is like a method (not a method) where it can set and get and validation can be added 
+            //ex:
+        //    public class Student
+        //{
+        //    private string name;
+        //    public string Name
+        //    {
+        //        get
+        //        {
+        //            return name;
+        //        }
+        //    }
+        //}
+        #endregion
+    }
     }
 }
